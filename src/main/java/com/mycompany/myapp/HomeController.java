70ea43e484e8c21b10d9b4cf8b51d46e581ed380
knm8643 <knm8643@nate.com>
2022-02-10 18:07:30 +0900

    home.jsp update_1

--- src/main/java/com/mycompany/myapp/HomeController.java
+++ src/main/java/com/mycompany/myapp/HomeController.java
@@ -1,18 +1,10 @@
 package com.mycompany.myapp;
 
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private HttpSession session;
 	
 	@RequestMapping(value = "/")
 	public String home() {
---
+++ src/main/java/com/mycompany/myapp/MadeController.java
@@ -0,0 +1,21 @@
+package com.mycompany.myapp;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("Made")
+public class MadeController {
+
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("mades")
+    public String Made() {
+		System.out.println("홈에서 Made호출 확인");
+		return "home";
+	}
+}
